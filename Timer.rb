3450fa9567811733015b9516bc569578cfbd30f4
AhsanTariqMajeed <ahsantariqmajeed@gmail.com>
2022-08-04 11:59:15 +0500

    Update Timer.rb

--- Timer.rb
+++ Timer.rb
@@ -1,20 +1,28 @@
-require"timeout"
+require 'timeout'
 require "csv"
-file = CSV.parse(File.read("problems.csv"))
 
-count = 0
+task = CSV.parse(File.read("problems.csv"))
 
-Timeout::timeout(10) { puts '10' }
-Timeout::timeout(5) do
-    for a in 0..file.length-1 do
-        puts "what is the answer of  #{file[a][0]} ?"
-        ans = gets
-        if file[a][1].chomp.to_i == ans.chomp.to_i
-            count=count+1
-        end
-    end
+def ask_question(question, answer, timer_answer)
+  puts "Please answer the question"
+  puts question
+  Timeout::timeout(timer_answer) do
+    user_ans = gets.chomp
+    return user_ans === answer
+  end
+rescue Timeout::Error
+  return false
 end
+
+timer_answer = 5
+score = 0
+
+for a in 0..task.length-1 do
+    correct = ask_question(file[a][0], task[a][1], timer_answer)
+    score += 1 if correct
+end
+
 puts "Total number of Questions:" 
-puts "#{file.length}"
-puts "Number of correct Answers: "
-puts "#{count}"
+puts "#{task.length}"
+puts "Number of correct Answers:"
+puts "#{score}"
